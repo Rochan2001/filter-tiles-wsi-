cf639770b2fc53072766129788412d9309528eae
Rochan <bahadur.rochan@gmail.com>
2021-08-04 17:35:58 +0530

    fixes

--- filter_thumb.py
+++ filter_thumb.py
@@ -41,7 +41,7 @@ class HelloWorld(object):
         min = int(min[0])*int(min[1])*int(min[2])
         max = int(max[0])*int(max[1])*int(max[2])
         print(min,max)
-        
+
         for file in fileList:
             img = Image.open(file)  # image extension *.png,*.jpg
             orgimg = img
@@ -56,10 +56,10 @@ class HelloWorld(object):
             colorval = pixelcolor[0][1][0] * pixelcolor[0][1][1] * pixelcolor[0][1][2]
             minColor=min
             maxColor=max
-            i=False 
+            i=False
             if colorval < maxColor and colorval >= minColor:
                 i=True
-            ff.append({'sno': str(ind), 'tile': file[-1],'rgb':str(pixelcolor[0][1][0]) +','+str(pixelcolor[0][1][1]) +','+ str(pixelcolor[0][1][2]),'r*g*b':str(colorval),'filtered':str(i)})    
+            ff.append({'sno': str(ind), 'tile': file[-1],'rgb':str(pixelcolor[0][1][0]) +','+str(pixelcolor[0][1][1]) +','+ str(pixelcolor[0][1][2]),'r*g*b':str(colorval),'filtered':str(i)})
             ind=ind+1
         with open('names.csv', 'w', newline='') as csvfile:
             fieldnames = ['sno', 'tile', 'rgb','r*g*b','filtered']
